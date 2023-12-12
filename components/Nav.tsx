ab03562246ab9deb2505c040c066a2a4d71e5a62
LevaniIlashvili <levaniko900@gmail.com>
2023-12-12 17:44:07 +0400

    mobile nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -13,13 +13,11 @@ import { BuiltInProviderType } from "next-auth/providers/index";
 
 const Nav = () => {
   const { data: session } = useSession();
-
   const [providers, setProviders] = useState<Record<
     LiteralUnion<BuiltInProviderType, string>,
     ClientSafeProvider
   > | null>(null);
-
-  console.log(session?.user);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
 
   useEffect(() => {
     const fetchProviders = async () => {
@@ -30,7 +28,7 @@ const Nav = () => {
   }, []);
 
   return (
-    <nav className="w-full flex justify-between items-center p-3 px-10 shadow-md">
+    <nav className="w-full flex justify-between items-center p-3 px-6 sm:px-10 shadow-md">
       <div className="flex gap-4">
         <Image
           src="/assets/images/logo.svg"
@@ -46,7 +44,8 @@ const Nav = () => {
       <div className="flex items-center gap-4 text-sm">
         {session?.user ? (
           <>
-            <div className="flex items-center gap-4">
+            {/* Desktop Navigation */}
+            <div className="hidden sm:flex items-center gap-4">
               <div className="flex gap-1">
                 <Image
                   className="fill-red-500"
@@ -57,18 +56,51 @@ const Nav = () => {
                 />
                 <button className="font-medium">Write</button>
               </div>
-              <button className="font-medium bg-black text-white rounded-full px-4 py-1">
+              <button
+                className="font-medium bg-black text-white rounded-full px-4 py-1"
+                onClick={() => signOut()}
+              >
                 Sign Out
               </button>
+              <Image
+                src={session?.user?.image}
+                alt="profile picture"
+                width={35}
+                height={35}
+                className="rounded-full"
+              />
             </div>
-            <Image
-              src={session?.user?.image}
-              alt="profile picture"
-              width={35}
-              height={35}
-              className="rounded-full"
-              onClick={() => signOut()}
-            />
+            {/* Mobile Navigation */}
+            <div className="relative sm:hidden">
+              <Image
+                src={session?.user?.image}
+                alt="profile picture"
+                width={35}
+                height={35}
+                className="rounded-full"
+                onClick={() => setToggleDropdown((prevState) => !prevState)}
+              />
+              {toggleDropdown && (
+                <div className="flex flex-col gap-2 absolute top-full right-0 bg-white w-32 p-2 mt-2 shadow-sm">
+                  <div className="flex gap-1">
+                    <Image
+                      className="fill-red-500"
+                      src="/assets/icons/write.svg"
+                      alt="write_icon"
+                      width={20}
+                      height={20}
+                    />
+                    <button className="font-medium">Write</button>
+                  </div>
+                  <button
+                    className="font-medium bg-black text-white rounded-full px-4 py-1"
+                    onClick={() => signOut()}
+                  >
+                    Sign Out
+                  </button>{" "}
+                </div>
+              )}
+            </div>
           </>
         ) : (
           <>
@@ -76,7 +108,7 @@ const Nav = () => {
               Object.values(providers).map((provider) => {
                 return (
                   <button
-                    className="font-medium bg-black text-white rounded-full px-4"
+                    className="font-medium bg-black text-white rounded-full px-4 py-1"
                     onClick={() => signIn(provider.id)}
                     key={provider.name}
                   >
