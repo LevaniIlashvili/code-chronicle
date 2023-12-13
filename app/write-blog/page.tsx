5437ef679285e26d7878fb198a45c7b0f00fbfb8
LevaniIlashvili <levaniko900@gmail.com>
2023-12-13 12:16:14 +0400

    page for writing blog

--- app/write-blog/page.tsx
+++ app/write-blog/page.tsx
@@ -1,18 +1,85 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { app } from "@/utils/firebase";
 
 const page = () => {
-  const [value, setValue] = useState("");
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
   const [image, setImage] = useState<File | null>(null);
+  const [imageURL, setImageURL] = useState("");
+
+  const storage = getStorage(app);
+
+  useEffect(() => {
+    const uploadImage = () => {
+      const name = new Date() + image?.name!;
+      const storageRef = ref(storage, name);
+
+      const uploadTask = uploadBytesResumable(storageRef, image!);
+
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {},
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageURL(downloadURL);
+          });
+        }
+      );
+    };
+
+    if (!image) return;
+    uploadImage();
+  }, [image]);
+
+  const publish = async (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    try {
+      console.log(title, content);
+      const res = await fetch("/api/blog/new", {
+        method: "POST",
+        body: JSON.stringify({ title, content, image: imageURL }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setTitle("");
+      setContent("");
+      setImage(null);
+      setImageURL("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <section className="flex flex-col items-center w-full p-4 mt-4">
-      <div className="flex flex-col w-[700px]">
+    <section className="flex justify-center w-full p-4 mt-4">
+      <div className="flex flex-col max-w-3xl gap-2 flex-1">
         <label
           htmlFor="media"
-          className="w-fit cursor-pointer bg-gray-300 p-1 mb-2 rounded-sm"
+          className="w-fit cursor-pointer bg-gray-300 p-1 px-2 mb-2 rounded-sm"
         >
           {image ? image.name : "Upload Image"}
         </label>
@@ -30,16 +97,24 @@ const page = () => {
         <input
           type="text"
           placeholder="Title"
-          className="text-4xl focus:outline-none"
+          className="w-full text-4xl focus:outline-none"
+          value={title}
+          onChange={(e) => setTitle((e.target as HTMLInputElement).value)}
         />
         <ReactQuill
           theme="bubble"
-          value={value}
-          onChange={setValue}
+          value={content}
+          onChange={setContent}
           placeholder="Tell your story..."
-          className="min-h-[300px] mt-4"
+          className="min-h-[50vh] w-full mt-4"
         />
-        <button className="bg-green-600 text-white font-semibold rounded-full w-fit p-1 px-4">
+        <p className="text-xs text-gray-500">
+          Highlight text to change it's style
+        </p>
+        <button
+          className="bg-green-600 text-white font-semibold rounded-full w-fit p-1 px-4"
+          onClick={publish}
+        >
           Publish
         </button>
       </div>
