da7ecf02bbd7ebb819d454b66f88ef842571d546
LevaniIlashvili <levaniko900@gmail.com>
2023-12-15 12:02:19 +0400

    route to get all blogs

---
+++ app/api/blog/route.ts
@@ -0,0 +1,12 @@
+import { connectToDb } from "@/utils/database";
+import { Blog } from "@/models/blog";
+
+export const GET = async () => {
+  try {
+    await connectToDb();
+    const blogs = await Blog.find({});
+    return new Response(JSON.stringify(blogs), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
