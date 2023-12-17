0663ec8d3a04088fe0a1c6e71afed521115a5dd8
LevaniIlashvili <levaniko900@gmail.com>
2023-12-17 10:56:08 +0400

    update blog schema to have creator property

--- models/blog.ts
+++ models/blog.ts
@@ -1,6 +1,10 @@
 import { Schema, model, models } from "mongoose";
 
 const BlogSchema = new Schema({
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
   title: {
     type: String,
     required: [true, "Title is required"],
