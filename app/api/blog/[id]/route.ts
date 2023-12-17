0e1af96add66fe1cc8d57251cc5e03699d01600f
LevaniIlashvili <levaniko900@gmail.com>
2023-12-17 19:29:35 +0400

    retrive single blog

---
+++ app/api/blog/[id]/route.ts
@@ -0,0 +1,14 @@
+import { Blog } from "@/models/blog";
+import { connectToDb } from "@/utils/database";
+
+export const GET = async (req: any, { params }: { params: { id: string } }) => {
+  try {
+    await connectToDb();
+
+    const blog = await Blog.findById(params.id).populate("creator");
+
+    return new Response(JSON.stringify(blog), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
