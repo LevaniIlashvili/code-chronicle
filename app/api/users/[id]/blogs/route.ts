ccfed890d28e8492ac0ba0a028b42ade5d739539
LevaniIlashvili <levaniko900@gmail.com>
2024-01-05 13:50:03 +0400

    add route to get users blogs

---
+++ app/api/users/[id]/blogs/route.ts
@@ -0,0 +1,12 @@
+import { Blog } from "@/models/blog";
+import { connectToDb } from "@/utils/database";
+
+export const GET = async (req: any, { params }: { params: { id: string } }) => {
+  try {
+    await connectToDb();
+    const blogs = await Blog.find({ creator: params.id }).populate("creator");
+    return new Response(JSON.stringify(blogs), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
